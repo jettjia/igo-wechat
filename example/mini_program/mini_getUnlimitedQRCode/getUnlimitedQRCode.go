e617ee5b41507cde5c3f8a55c375a0d8d1db8352
“jettjia” <jettjia@foxmail.com>
2022-10-15 15:59:46 +0800

    fate:小程序推广码

--- example/mini_program/mini_getUnlimitedQRCode/getUnlimitedQRCode.go
+++ example/mini_program/mini_getUnlimitedQRCode/getUnlimitedQRCode.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-
+	"github.com/gogf/gf/v2/os/gfile"
 	"github.com/jettjia/wechat/example/consts"
 	"github.com/jettjia/wechat/sdk/mini_program"
 	v1 "github.com/jettjia/wechat/service/mini_program/v1"
-	"github.com/jettjia/wechat/util"
+	"path"
 )
 
-// 获取不限制的小程序码
+// 获取不限制的小程序个人推广码
 // https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/qrcode-link/qr-code/getUnlimitedQRCode.html
 func main() {
 	conf := mini_program.Config{
@@ -21,7 +21,15 @@ func main() {
 	scene := "g_user_id=100"    // 传递给小程序的参数
 	page := "pages/index/index" // 接收者的 openid
 	envVersion := "develop"     // 要打开的小程序版本。正式版为 "release"，体验版为 "trial"，开发版为 "develop"。默认是正式版
-	rsp := client.GetUnlimitedQRCode(scene, page, envVersion)
+	buffer := client.GetUnlimitedQRCode(scene, page, envVersion)
 
-	fmt.Println("data: ", util.PrintJson(rsp))
+	//fmt.Println("data: ", util.PrintJson(buffer))
+
+	// 生成图片保存
+	filename := "promotion.png"
+	filepath := path.Join("./", filename)
+	err := gfile.PutContents(filepath, buffer)
+	if err != nil {
+		fmt.Println("保存推广图片失败")
+	}
 }
